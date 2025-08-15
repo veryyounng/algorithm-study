f7422bb0b14f68c5400df0fc08a59385d236cb74
유성원 <swyoo753@naver.com>
2025-08-15 17:30:05 +0900

    자물쇠와열쇠

--- BJ/week1/seongwon/이코테/문자열압축.py
+++ BJ/week1/seongwon/이코테/문자열압축.py
@@ -12,7 +12,10 @@ for step in range(1, len(s)//2+1):
         if prev == s[j:j+step]:
             count += 1
         else:
-            compressed += str(count) + prev if count >= 2 else prev
+            if count >= 2:
+                compressed += str(count) + prev
+            else:
+                compressed += prev
             prev = s[j:j+step]
             count = 1
     compressed += str(count) + prev if count >= 2 else prev
