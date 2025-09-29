7df0866890158846e6c7bf3b71067d00c46e34d9
veryyounng <veryyoung811@gmail.com>
2025-09-29 21:56:22 +0900

    12919 A와 B 2 재풀이 완료

--- BJ/25_09_week5/soyoung/12919_A와B2.py
+++ BJ/25_09_week5/soyoung/12919_A와B2.py
@@ -1,14 +1,15 @@
-
-s = input().strip()
-t = input().strip()
-
-def game():
-    cur = t
-    while len(cur) > len(s):
-        if cur[-1] == 'A':
-            cur = cur[:-1]
-        else:  # 마지막이 'B'면 B를 떼고 뒤집기
-            cur = cur[:-1][::-1]
-    return 1 if cur == s else 0
-
-print(game())
+import sys
+S=list(input())
+T=list(input())
+def dfs(t): # 문자열 T 리스트를 입력받아
+    if t==S:
+        print(1)
+        sys.exit()
+    if len(t)==0:
+        return 0
+    if t[-1]=='A': # 마지막이 A이면 
+        dfs(t[:-1]) # 제거해서 재귀
+    if t[0]=='B': # 처음이 B이면
+        dfs(t[1:][::-1]) # B제거하고, 뒤집어서 재귀
+dfs(T)
+print(0)
